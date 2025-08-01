9aceaf7ae71aa5ad02d8103d9b5eedb7f8cff0a5
Suryarghya Saha <20je0998@cse.iitism.ac.in>
2025-08-01 23:53:13 +0530

    header light-mode

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
-import Image from "next/image";
+import TodoTable from "@/components/TodoTable";
+import ToggleButton from "@/components/ToggleButton";
 
 export default function Home() {
-  return <></>;
+  return (
+    <div className="min-h-screen bg-[url('/bg-desktop-light.jpg')] bg-no-repeat bg-fixed bg-top bg-contain bg-slate-200 relative font-josefin-custom">
+      <ToggleButton />
+      <TodoTable />
+    </div>
+  );
 }
---
+++ components/Header.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Image from "next/image";
+
+export default function Header() {
+  return (
+    <div className="bg-[url('/bg-desktop-light.jpg')] h-[18rem] bg-fixed bg-no-repeat"></div>
+  );
+}
