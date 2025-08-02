4a08ceee39d4086dd35cf5f15dfc34172d12e088
Suryarghya Saha <20je0998@cse.iitism.ac.in>
2025-08-02 09:17:22 +0530

    dummy useState

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,10 @@ import ToggleButton from "@/components/ToggleButton";
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-[url('/bg-desktop-light.jpg')] bg-no-repeat bg-fixed bg-top bg-contain bg-slate-200 relative font-josefin-custom flex flex-col items-center pt-[10rem]">
+    <div
+      className="min-h-screen bg-[url('/bg-desktop-light.jpg')] bg-no-repeat bg-fixed bg-top bg-contain bg-slate-200 
+                      relative font-josefin-custom flex flex-col items-center pt-[10rem]"
+    >
       <ToggleButton />
       <TodoTable />
       <StatusBar />
